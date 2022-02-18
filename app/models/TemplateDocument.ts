3550f799f3f7c6437f15d90e356622b510bec661
Paul Chavard <pro@paul.chavard.net>
2022-02-18 13:01:33 +0100

    fix(email-template): fix type

--- app/models/TemplateDocument.ts
+++ app/models/TemplateDocument.ts
@@ -76,10 +76,10 @@ export type CustomElement =
 export function isList(node: Block): node is NumberedList | BulletedList {
   return node.type == 'numbered-list' || node.type == 'bulleted-list';
 }
-export function isTag(node: { type?: string }): node is Tag {
+export function isTag(node: { type?: string; text?: string }): node is Tag {
   return 'type' in node && node.type == 'tag';
 }
-export function isLink(node: { type?: string }): node is Link {
+export function isLink(node: { type?: string; text?: string }): node is Link {
   return 'type' in node && node.type == 'link';
 }
 export function isText(node: { text?: string }): node is FormattedText {
